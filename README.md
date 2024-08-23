b798b3ce928f0d5633ad38c49633284eb0923011
J4yyy <justbrin7@gmail.com>
2024-08-23 13:12:16 +0200

    Setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# GuessTheNumber
+This very small CLI Game is my first step in Rust Development
+
+
+## Features
+* It generates a random number between 0 and 101
+* The Console aks for your Input and compares it to the Secret Number
+* It keeps track of your missed guesses
---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use std::cmp::Ordering;
+use std::io;
+use std::process::Command;
+use rand::Rng;
+
+fn main() {
+    println!("Welcome to guess the number");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut guess_counter: u16 = 0;
+
+    loop {
+        println!("Please input your guess");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => {
+                num
+            }
+            Err(_) => {
+                println!("Please Enter valid Number!");
+                continue;
+            }
+        };
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("To small"),
+            Ordering::Greater => println!("To tall"),
+            Ordering::Equal => {
+                if cfg!(target_os = "windows") {
+                    Command::new("cls")
+                        .status().expect("Failed");
+                } else {
+                    Command::new("Clear")
+                        .status().expect("Failed");
+                }
+                println!("You guessed it! The Secret Number was {}", secret_number);
+                println!("You took {} guesses to find the number", guess_counter +1);
+                break;
+            }
+        }
+        guess_counter += 1;
+    }
+}
